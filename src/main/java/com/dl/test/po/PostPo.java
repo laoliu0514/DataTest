d98973cb98291494f5ade7e38eb87e6899a0e812
laoliu0514 <laoliu0514@sina.com>
2017-11-20 18:57:16 +0800

    PostPo类添加了一些信息

--- src/main/java/com/dl/test/po/PostPo.java
+++ src/main/java/com/dl/test/po/PostPo.java
@@ -3,4 +3,24 @@ package com.dl.test.po;
 public class PostPo {
 	
 	private String num;
+	private int id;
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "PostPo [num=" + num + ", id=" + id + "]";
+	}
+	
+	
+	
 }
